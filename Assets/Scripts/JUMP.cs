4ea31947e845dda5851cb6465c63020a612482b4
AzureAD\GUILLEMSALASSISTACHS <guillem.salas@enti.cat>
2024-02-12 19:59:02 +0100

    salto intento

--- Assets/Scripts/JUMP.cs
+++ Assets/Scripts/JUMP.cs
@@ -9,56 +9,37 @@ public class JUMP : MonoBehaviour
     private float coyoteTimeCounter;
 
     [SerializeField] private float _jumpForce = 16f;
-    [SerializeField] private float _jumpMultiplier = 0.5f;
+    [SerializeField] private float _jumpForceAug = 16f;
     [SerializeField] private float _jumpBufferTime = 0.2f;
     [SerializeField] private float _jumpBufferCounter;
     [SerializeField] private bool _isJumping;
     [SerializeField] private float _jumpCooldown = 0.4f; 
-    public Rigidbody2D rb;
+    Rigidbody2D rb;
 
-    private void FixedUpdate()
+    PlayerInput _input;
+    private void Start()
     {
-        if (PhysicsManager.Instance.IsGrounded)
-        {
-            coyoteTimeCounter = coyoteTime;
-        }
-        else
-        {
-            coyoteTimeCounter -= Time.fixedDeltaTime;
-        }
-    }
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            _jumpBufferCounter = _jumpBufferTime;
-        }
-        else
-        {
-            _jumpBufferCounter -= Time.fixedDeltaTime;
-        }
-
-        if (coyoteTimeCounter > 0f && _jumpBufferCounter > 0f && !_isJumping)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, _jumpForce);
-
-            _jumpBufferCounter = 0f;
-
-            StartCoroutine(JumpCooldown());
-        }
-        if (context.started && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * _jumpMultiplier);
-
-            coyoteTimeCounter = 0f;
-        }
+        _input = new PlayerInput();
+        _input.Player.Enable();
         
+        _input.Player.Jump.performed += Jump_performed;
+        _input.Player.Jump.canceled += Jump_canceled;
+
+        rb = GetComponent<Rigidbody2D>();
     }
-    private IEnumerator JumpCooldown()
+
+    private void Jump_performed(InputAction.CallbackContext context)
     {
-        _isJumping = true;
-        yield return new WaitForSeconds(_jumpCooldown);
-        _isJumping = false;
+        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
     }
+
+    private void Jump_canceled(InputAction.CallbackContext context)
+    {
+        if (PhysicsManager.Instance.IsGrounded == true)
+        {
+            Debug.Log("Salto1");
+            rb.velocity = new Vector2(rb.velocity.x, _jumpForce);
+        }
+    }
+
 }
