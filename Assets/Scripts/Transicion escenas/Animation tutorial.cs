55b31fa9f17b11f1b58231d1666656c44677422b
DESKTOP-K73ES4O\leipo <pol.santos@enti.cat>
2024-05-08 21:58:45 +0200

    dialogo tutorial empezado

--- Assets/Scripts/Transicion escenas/Animation tutorial.cs
+++ Assets/Scripts/Transicion escenas/Animation tutorial.cs
@@ -1,15 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Animationtutorial : MonoBehaviour
 {
     [SerializeField] GameObject bg_top;
     [SerializeField] GameObject bg_down;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField, TextArea(4, 6)] private string[] dialogueLines;
+
+    private float typingTime = 0.05f;
+
+    private bool didDialogueStart;
+    private int lineIndex;
+
+    private bool canPassDialogue;
+    private bool animationEnded = false;
 
     private void Start()
     {
         StartAnimation();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (animationEnded)
+            {
+                StartDialogue();
+            }
+            else if (dialogueText.text == dialogueLines[lineIndex]) ;
+            {
+                NextDialogueLine();
+            }
+        }
     }
 
 
@@ -17,12 +43,43 @@ public class Animationtutorial : MonoBehaviour
     {
         LeanTween.moveY(bg_top.GetComponent<RectTransform>(), 180, 1f).setEase(LeanTweenType.easeOutCubic);
         LeanTween.moveY(bg_down.GetComponent<RectTransform>(), -180, 1f).setEase(LeanTweenType.easeOutCubic)
-            .setOnComplete(EndAnimation);
+            .setOnComplete(StartDialogue);
+        animationEnded = true;
     }
 
     private void EndAnimation()
     {
         LeanTween.moveY(bg_top.GetComponent<RectTransform>(), 265, 1.2f);
         LeanTween.moveY(bg_down.GetComponent<RectTransform>(), -265, 1.2f);
+    }
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        lineIndex = 0;
+        StartCoroutine(ShowLine());
+    }
+
+    public void NextDialogueLine()
+    {
+        lineIndex++;
+        if (lineIndex < dialogueLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            didDialogueStart = false;
+        }
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+        yield return new WaitForSeconds(1.2f);
+        foreach (char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSeconds(typingTime);
+        }
     }
 }
