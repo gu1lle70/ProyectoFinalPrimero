b0ba2bc82507bea56fa90cb7656d2246a57dfaa8
DESKTOP-K73ES4O\leipo <pol.santos@enti.cat>
2024-06-04 23:49:06 +0200

    acabando

--- Assets/Scripts/Transicion escenas/SceneTransitions.cs
+++ Assets/Scripts/Transicion escenas/SceneTransitions.cs
@@ -8,26 +8,25 @@ public class SceneTransitions : MonoBehaviour
     public static SceneTransitions Instance;
     [SerializeField] private GameObject black_bg;
     [SerializeField] private string SceneName;
-    [SerializeField] private GameObject NullObject;
+
 
    
 
     private void BajarAlpha()
     {
+        black_bg.SetActive(true);
         LeanTween.alpha(black_bg.GetComponent<RectTransform>(), 2f, 1f).setOnComplete(ChangeScene);
         return;
     }
 
-    public void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (NullObject == null)
-        {
-            BajarAlpha();
-        }
+        BajarAlpha();
     }
 
     private void ChangeScene()
     {
+
         SceneManager.LoadScene(SceneName);
     }
 
